70bc11abebbf81992fc57e0264376229a627bc6d
Utkarsh <utkarsh.goyal.15cse@bml.edu.in>
2022-05-25 17:22:58 +0200

    change in checkExpiry

--- helpers/checkExpiry.js
+++ helpers/checkExpiry.js
@@ -3,4 +3,4 @@
 exports.getExpiryTimestamp = ttl => Date.now() + ttl;
 
 // To check the cache is expired or not
-exports.isItemExpired = expiryDate => expiryDate < Date.now();
+exports.isItemExpired = expiryDate => expiryDate > Date.now();
